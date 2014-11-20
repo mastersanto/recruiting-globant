20bc2d6447b50744ee8ad1eb05e61647c4fc3b12
Jorge Sandoval <jorgeivansandoval@gmail.com>
2014-11-20 07:08:05 -0500

    Hero img height and positions styles

--- sites/all/themes/custom/globant_recruiting/js/main.js
+++ sites/all/themes/custom/globant_recruiting/js/main.js
@@ -11,8 +11,10 @@ var GLB = GLB || {};
 GLB.sectionResize = {
 	init: function(ev, $) {
 		var $sections = jQuery('.glb-section'),
+			$heroImg = $('.hero-container img'),
 			sectionMinHeight = ev.target.innerHeight + 'px';
 		$sections.css('min-height', sectionMinHeight);
+		$heroImg.css('min-height', sectionMinHeight);
 	}
 }
 
