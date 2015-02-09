da5be4fd819636822785c6df6e18608235dad32f
lmparra <neftons@gmail.com>
2015-02-09 18:34:34 -0500

    Highlight image print title

--- sites/all/modules/custom/image_hero/templates/image-hero-block.tpl.php
+++ sites/all/modules/custom/image_hero/templates/image-hero-block.tpl.php
@@ -1,4 +1,4 @@
-<h1 class="page-title"><span><?php print t('En Globant hace carrera'); ?></span></h1>
+<h1 class="page-title"><span><?php print $variables['title']; ?></span></h1>
 <div class="hero-container">
   <?php print $variables['image']; ?>
 </div>
