01c57acb7601d9d137ec3a834a63ae3195c00e57
lmparra <neftons@gmail.com>
2015-02-11 06:42:14 -0500

    Add subtitle to Videos modal

--- sites/all/modules/custom/globant_studios/templates/globant-studios-block.tpl.php
+++ sites/all/modules/custom/globant_studios/templates/globant-studios-block.tpl.php
@@ -8,7 +8,7 @@
       <a href="#" class="studio-content" data-toggle="modal" data-target="#video<?php print $key ?>">
         <div class="thumbnail-wrapper"><?php print $node['thumb'] ?></div>
         <span class="studio-overlay">
-            <?php print $node['title'][0]; print !empty($node['title'][1]) ? '<br />' . $node['title'][1] : ''; ?>
+            <?php print $node['title'] ?><?php if($node['subtitulo']): ?><span class="video-author"><?php print $node['subtitulo'] ?></span><?php endif; ?>
         </span>
       </a>
     </div>
@@ -20,12 +20,13 @@
   jQuery(document).ready(function($) {
     // Reset every player
     $('.close').click(function() {
+      var modal = $(this).closest('.modal-content');
       //First get the  iframe URL
-      var url = $('iframe.media-youtube-player').attr('src');
+      var url = modal.find('iframe').attr('src');
       //Then assign the src to null, this then stops the video been playing
-      $('iframe.media-youtube-player').attr('src', '');
+      modal.find('iframe').attr('src', '');
       // Finally you reasign the URL back to your iframe, so when you hide and load it again you still have the link
-      $('iframe.media-youtube-player').attr('src', url);
+      modal.find('iframe').attr('src', url);
     });
   });
 </script>
@@ -36,7 +37,10 @@
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
           <div class="modal-column-left">
-            <h4 class="modal-title" id="myModalLabel"><?php print $node['title'][0]; print !empty($node['title'][1]) ? '<br />' . $node['title'][1] : ''; ?></h4>
+            <h4 class="modal-title" id="myModalLabel">
+              <?php print $node['title'] ?>
+              <?php if($node['subtitulo']): ?><span class="video-author"><?php print $node['subtitulo'] ?></span><?php endif; ?>
+            </h4>
           </div>
         </div>
         <div class="modal-body">
